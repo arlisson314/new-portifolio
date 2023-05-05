73e7e13e8fdb476c8c7d3cc0721fff2bbc1f0825
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-05 09:32:34 -0300

    feat: adicona keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        showSidebar: {
+          from: { opacity: 0, width: 0 },
+          to: { opacity: 1 },
+        },
+      },
+      animation: {
+        showSidebar: 'showSidebar 1s ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
