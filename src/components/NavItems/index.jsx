48b2d1080176568c17e54e4af5e9ab7f4aac2491
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-05 10:04:18 -0300

    feat: cria componente NavItems

--- src/components/NavItems/index.jsx
+++ src/components/NavItems/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 
-export default function HeaderItem({ Icon, children }) {
+export default function NavItems({ Icon, children, renderSiderbar }) {
   return (
     <Link
       to={children}
@@ -9,6 +9,8 @@ export default function HeaderItem({ Icon, children }) {
       smooth
       offset={0}
       duration={500}
+      onClick={renderSiderbar && renderSiderbar}
+      className="flex justify-center items-center space-x-1 cursor-pointer text-lg hover:text-blue-400"
     >
       <Icon />
       <p>{children}</p>
