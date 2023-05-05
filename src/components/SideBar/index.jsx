bf065aa8c98034dfdfacec6468f936fc4783fe51
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-05 10:05:38 -0300

    feat: cria comonente Sidebar e adicona componente NavItems

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,22 @@
+import {
+  FaTimes,
+  FaHome,
+  FaEnvelope,
+  FaUserAlt,
+  FaIdCardAlt,
+} from 'react-icons/fa';
+import NavItems from '../NavItems';
+
+export default function Sidebar({ renderSiderbar }) {
+  return (
+    <nav className="w-full h-full flex flex-col justify-center items-end z-40 bg-white fixed left-0 top-0 animate-showSidebar">
+      <FaTimes onClick={renderSiderbar} size={20} className="h-16 mr-5 mt-2" />
+      <div className="w-full h-full flex flex-col space-y-12 mt-16 items-center">
+        <NavItems Icon={FaHome} renderSiderbar={renderSiderbar}>Home</NavItems>
+        <NavItems Icon={FaUserAlt} renderSiderbar={renderSiderbar}>About</NavItems>
+        <NavItems Icon={FaIdCardAlt} renderSiderbar={renderSiderbar}>Projects</NavItems>
+        <NavItems Icon={FaEnvelope} renderSiderbar={renderSiderbar}>Contact</NavItems>
+      </div>
+    </nav>
+  );
+}
