8849d11e5bbe0fd2c386415d708ee40258502648
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-08 17:01:30 -0300

    feat: Cria componente Projects

---
+++ src/MockProjects/index.jsx
@@ -0,0 +1,14 @@
+import pixeArt from '../images/pixelArt.png';
+
+const projectList = [
+  {
+    name: 'Pixe art',
+    imagem: pixeArt,
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, alias sed? Facere, magni eaque, placeat dignissimos assumenda officiis ab pariatur quaerat obcaecati deleniti exercitationem reiciendis porro sed corrupti saepe? Nostrum!',
+    tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
+    previa: 'https://pixels-art-bay.vercel.app/',
+    repostirio: 'https://github.com/arlisson314/pixels-art',
+  },
+];
+
+export default projectList;
