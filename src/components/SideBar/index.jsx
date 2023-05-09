432f5aa56d4bd1f7f4899c363ccf2f149173923f
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-09 16:22:50 -0300

    feat: adicona texto em portugues nos links do sidebar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -12,10 +12,10 @@ export default function Sidebar({ renderSiderbar }) {
     <nav className="w-full h-full flex flex-col justify-center items-end z-40 bg-white fixed left-0 top-0 animate-showSidebar">
       <FaTimes onClick={renderSiderbar} size={20} className="h-16 mr-5 mt-2" />
       <div className="w-full h-full flex flex-col space-y-12 mt-16 items-center">
-        <NavItems Icon={FaHome} renderSiderbar={renderSiderbar}>Home</NavItems>
-        <NavItems Icon={FaUserAlt} renderSiderbar={renderSiderbar}>About</NavItems>
-        <NavItems Icon={FaIdCardAlt} renderSiderbar={renderSiderbar}>Projects</NavItems>
-        <NavItems Icon={FaEnvelope} renderSiderbar={renderSiderbar}>Contact</NavItems>
+        <NavItems Icon={FaHome} renderSiderbar={renderSiderbar}>Inicio</NavItems>
+        <NavItems Icon={FaUserAlt} renderSiderbar={renderSiderbar}>Sobre</NavItems>
+        <NavItems Icon={FaIdCardAlt} renderSiderbar={renderSiderbar}>Projetos</NavItems>
+        <NavItems Icon={FaEnvelope} renderSiderbar={renderSiderbar}>Contatos</NavItems>
       </div>
     </nav>
   );
