16e2cb91d9a27294dd08e47aa73e6c468dc95854
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-09 16:21:58 -0300

    feat: cria mock de dados dos projetos

--- src/MockProjects/index.jsx
+++ src/MockProjects/index.jsx
@@ -1,7 +1,36 @@
 import pixeArt from '../images/pixelArt.png';
+import myShopping from '../images/myShopping.png';
 
 const projectList = [
   {
+    id: 1,
+    name: 'My shopping',
+    imagem: pixeArt,
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, alias sed? Facere, magni eaque, placeat dignissimos assumenda officiis ab pariatur quaerat obcaecati deleniti exercitationem reiciendis porro sed corrupti saepe? Nostrum!',
+    tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
+    previa: 'https://my-shopping-alpha.vercel.app/',
+    repostirio: 'https://github.com/arlisson314/My-shopping',
+  },
+  {
+    id: 2,
+    name: 'Pixe art',
+    imagem: myShopping,
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, alias sed? Facere, magni eaque, placeat dignissimos assumenda officiis ab pariatur quaerat obcaecati deleniti exercitationem reiciendis porro sed corrupti saepe? Nostrum!',
+    tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
+    previa: 'https://pixels-art-bay.vercel.app/',
+    repostirio: 'https://github.com/arlisson314/pixels-art',
+  },
+  {
+    id: 3,
+    name: 'Pixe art',
+    imagem: pixeArt,
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, alias sed? Facere, magni eaque, placeat dignissimos assumenda officiis ab pariatur quaerat obcaecati deleniti exercitationem reiciendis porro sed corrupti saepe? Nostrum!',
+    tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
+    previa: 'https://pixels-art-bay.vercel.app/',
+    repostirio: 'https://github.com/arlisson314/pixels-art',
+  },
+  {
+    id: 4,
     name: 'Pixe art',
     imagem: pixeArt,
     descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, alias sed? Facere, magni eaque, placeat dignissimos assumenda officiis ab pariatur quaerat obcaecati deleniti exercitationem reiciendis porro sed corrupti saepe? Nostrum!',
