ca6a8fde4e76ca1ba21b7c908297883464761164
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-10 11:33:44 -0300

    feat: Adiciona estilo

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,7 +2,7 @@ import developer from '../../images/perfil/developer.png';
 
 export default function About() {
   return (
-    <div id="Sobre" className="w-full h-[calc(100vh_-_64px)] mt-20 flex flex-col items-ceter sm:flex-row sm:items-center sm:justify-center sm:space-x-4">
+    <div id="Sobre" className="w-full h-[calc(100vh_-_64px)] mt-20 flex flex-col items-ceter sm:flex-row sm:items-center sm:justify-center sm:space-x-4 sm:mt-0">
       <img src={developer} alt="imagem pessoa programdora" className="w-60 h-60 mx-auto rounded-md sm:w-80 sm:h-72 sm:mx-0" />
       <div className="w-4/5 h-2/5 mt-4 text-left mx-auto sm:w-1/3 sm:mx-0 sm:h-2/5 sm:flex sm:flex-col sm:justify-center sm:mt-0">
         <h3 className="text-blue-400 font-semibold">Sobre mim</h3>
