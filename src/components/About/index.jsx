b5867f76d0a42170c37562e990240f2093eb74b9
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-10 15:56:49 -0300

    feat: Adiciona estilo

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div id="Sobre" className="w-full h-[calc(100vh_-_64px)] mt-20 flex flex-col items-ceter sm:flex-row sm:items-center sm:justify-center sm:space-x-20 sm:mt-0">
       <img src={developer} alt="imagem pessoa programdora" className="w-60 h-60 mx-auto rounded-md sm:w-80 sm:h-72 sm:mx-0" />
       <div className="w-4/5 h-2/5 mt-4 text-left mx-auto sm:w-1/3 sm:mx-0 sm:h-2/5 sm:flex sm:flex-col sm:justify-center sm:mt-0">
-        <h3 className="text-blue-400 font-semibold">Sobre mim</h3>
+        <h3 className="text-blue-500 font-semibold">Sobre mim</h3>
         <h2 className="font-semibold text-lg">Desenvolvedor Full Stack formado pela escola Trybe.</h2>
         <p className="text-justify">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
