0dfc112c3eae1b0858f5b60875e8448c36625bd5
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-11 10:29:34 -0300

    feat: Adiciona estilos para darkMode

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -8,22 +8,22 @@ const gitHub = 'https://github.com/arlisson314';
 
 export default function Home() {
   return (
-    <div id="Inicio" className="flex flex-col-reverse w-full h-[calc(100vh_-_64px)] items-center mt-20 space-x-4 sm:flex-row sm:justify-center sm:mt-0">
+    <div id="Inicio" className="flex flex-col-reverse w-full h-[calc(100vh_-_64px)] items-center mt-16 pt-2 space-x-4 sm:flex-row sm:justify-center sm:mt-0">
       <main className="w-full px-4 h-2/3 flex flex-col justify-center items-center space-y-10 sm:w-3/6">
-        <h1 className="flex text-5xl justify-center items-center w-min font-bold">
+        <h1 className="flex text-5xl justify-center items-center w-min font-bold text-slate-950 dark:text-gray-200">
           FullStack Developer
           <img src={hand} alt="hand" className="w-14 h-min ml-3 mt-10" />
         </h1>
-        <p>
+        <p className="text-gray-600">
           Olá, sou Arlisson Nascimento. Um desenvolvedor apaixonado por codigo.📍
-          <span className="flex mt-2 sm:mt-4 space-x-2">
-            <a href={gitHub} aria-label="link github" target="_blank" rel="noreferrer"><RxGithubLogo className="text-2xl text-gray-600 hover:text-blue-500 transition ease-in-out duration-500" /></a>
-            <a href={linkdin} aria-label="link github" target="_blank" rel="noreferrer"><RxLinkedinLogo className="text-2xl text-gray-600 hover:text-blue-500 transition ease-in-out duration-500" /></a>
+          <span className="flex mt-2 sm:mt-4 space-x-2 text-slate-950 dark:text-gray-600">
+            <a href={gitHub} aria-label="link github" target="_blank" rel="noreferrer"><RxGithubLogo className="text-2xl hover:text-blue-500 transition ease-in-out duration-500" /></a>
+            <a href={linkdin} aria-label="link github" target="_blank" rel="noreferrer"><RxLinkedinLogo className="text-2xl hover:text-blue-500 transition ease-in-out duration-500" /></a>
           </span>
         </p>
       </main>
       <div className="w-4/6 h-2/3 flex justify-center items-center sm:w-2/6">
-        <div className="perfil" />
+        <div className="perfil dark:border-gray-600" />
       </div>
 
     </div>
