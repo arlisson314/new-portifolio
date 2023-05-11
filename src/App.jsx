98f07b6b7c6c26aec071453f49a2b4a955b14171
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-10 21:18:21 -0300

    feat: Adiciona componente Contact ao app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import About from './components/About';
 import Header from './components/Header';
 import Home from './components/Home';
+import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
