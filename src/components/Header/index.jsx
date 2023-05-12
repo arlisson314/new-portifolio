5782b7e5f2b734e8353c9e1843409329bd7de760
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-12 09:04:26 -0300

    feat: Adicona funcionalidade para alterara tema entre dark e light

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,19 +4,21 @@ import {
   FaUserAlt,
   FaIdCardAlt,
   FaEnvelope,
+  FaRegMoon,
+  FaRegLightbulb,
 } from 'react-icons/fa';
 
 import { useState } from 'react';
 import useWindowSize from '../hooks/useWindowSize';
 import Sidebar from '../SideBar';
 import NavItems from '../NavItems';
+import { useTheme } from '../hooks/useTheme';
 
 export default function Header() {
   const windowSize = useWindowSize();
-
   const [sidebar, setSidebar] = useState(false);
-
   const renderSiderbar = () => setSidebar(!sidebar);
+  const { theme, setTheme } = useTheme();
 
   return (
     <header className="flex justify-between bg-gray-100 dark:bg-slate-950 dark:text-gray-200 border-b-2 dark:border-b-0 items-center h-16 px-4 z-10 w-full fixed top-0">
@@ -30,6 +32,19 @@ export default function Header() {
           <NavItems Icon={FaUserAlt}>Sobre</NavItems>
           <NavItems Icon={FaIdCardAlt}>Projetos</NavItems>
           <NavItems Icon={FaEnvelope}>Contatos</NavItems>
+          {theme === 'light' ? (
+            <FaRegMoon
+              size={23}
+              className="cursor-pointer text-blue-500"
+              onClick={() => setTheme('dark')}
+            />
+          ) : (
+            <FaRegLightbulb
+              size={23}
+              className="cursor-pointer text-blue-500"
+              onClick={() => setTheme('light')}
+            />
+          )}
         </nav>
       ) : (<FaBars size={25} onClick={renderSiderbar} />)}
     </header>
