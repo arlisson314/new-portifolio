ef04083e9386d57e730ff487e1f9e84a9116f503
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-15 16:50:49 -0300

    feat: Incrementa texto da Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -10,14 +10,17 @@ export default function Home() {
   const gitHub = 'https://github.com/arlisson314';
 
   return (
-    <div id="Inicio" className="flex flex-col-reverse w-full h-[calc(100vh_-_64px)] items-center mt-16 pt-2 space-x-4 sm:flex-row sm:justify-center sm:mt-0">
-      <main className="w-full px-4 h-2/3 flex flex-col justify-center items-center space-y-10 sm:w-3/6">
-        <h1 className="flex text-5xl justify-center items-center w-min font-bold text-slate-950 dark:text-gray-200">
+    <div id="Inicio" className="flex flex-col-reverse w-full h-auto sm:h-[calc(100vh_-_64px)] items-center mt-16 pt-2 space-x-4 sm:flex-row sm:justify-center sm:mt-0">
+      <main className="w-full px-4 h-2/3 flex flex-col justify-center items-center space-y-5 sm:w-3/6">
+        <h1 className="flex text-5xl justify-center items-center w-min font-bold text-slate-950 dark:text-gray-200 mt-2">
           FullStack Developer
           <img src={hand} alt="hand" className="w-14 h-min ml-3 mt-10" />
         </h1>
         <p className="text-slate-950 dark:text-gray-500">
-          Olá, sou Arlisson Nascimento. Um desenvolvedor apaixonado por codigo.📍
+          Olá, sou Arlisson Nascimento. Um desenvolvedor apaixonado por codigo.
+          <br />
+          Bem-vindo ao meu portfólio! Aqui você encontrará alguns dos meus melhores projetos,
+          além de informações sobre minhas habilidades e tecnologias que utilizo.📍
           <span className="flex mt-2 sm:mt-4 space-x-2">
             <a href={gitHub} aria-label="link github" target="_blank" rel="noreferrer"><RxGithubLogo className="text-2xl hover:text-blue-500 transition ease-in-out duration-500" /></a>
             <a href={linkdin} aria-label="link github" target="_blank" rel="noreferrer"><RxLinkedinLogo className="text-2xl hover:text-blue-500 transition ease-in-out duration-500" /></a>
