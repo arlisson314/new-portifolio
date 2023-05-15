5ab23e9fc617f1ecddc81c62b9c7439e15460571
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-15 16:05:28 -0300

    feat: Adiciona estilo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-slate-950 text-gray-200 w-full h-10 flex justify-center items-center sm:justify-start sm:pl-10">
+    <footer className="bg-slate-950 text-gray-600 w-full h-10 flex justify-center items-center sm:justify-start sm:pl-10">
       <span>Copyright © 2023. All rights reserved.</span>
     </footer>
   );
