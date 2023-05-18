4f926201f6e09716a8ae6310522b0b269f54264c
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-17 22:15:02 -0300

    feat: Configura keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,21 @@ export default {
           from: { opacity: 0, width: 0 },
           to: { opacity: 1 },
         },
+        bye: {
+          0: {
+            transform: 'rotate(0deg)',
+          },
+          '50%': {
+            transform: 'rotate(45deg)',
+          },
+          '100%': {
+            transform: 'rotate(0deg)',
+          },
+        },
       },
       animation: {
         showSidebar: 'showSidebar 1s ease-in-out',
+        bye: 'bye 2s infinite ease-in-out',
       },
     },
   },
