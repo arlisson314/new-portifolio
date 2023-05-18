b342f57fbd24e7e9e2c5155a03c70307ba836d28
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-18 18:25:07 -0300

    feat: Adiciona emotions

--- src/MockProjects/index.jsx
+++ src/MockProjects/index.jsx
@@ -4,7 +4,7 @@ import myShopping from '../images/myShopping.png';
 const projectList = [
   {
     id: 1,
-    name: 'My shopping',
+    name: 'My shopping 🛒',
     imagem: myShopping,
     descricao: 'Este é um projeto simples de carrinho de compras construído com JavaScript puro. Essa aplicação foi construída consumindo dados da API do Mercado Livre. O aplicativo permite que os usuários adicionem ou removam itens ao seu carrinho de compras, visualizem os itens em seu carrinho e calculem o custo total do pedido',
     tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
@@ -13,7 +13,7 @@ const projectList = [
   },
   {
     id: 2,
-    name: 'Pixel art',
+    name: 'Pixel art 🎨',
     imagem: pixeArt,
     descricao: 'Um projeto que simula um "canva" na tela. Existem 4 cores em uma paleta geradas automaticamente na inicialização da página. É possível alterar a quantidade de "pixels", e ao clicar sobre um "pixel"  é possível pintá-lo com a cor selecionada da paleta (preta caso nenhuma tenha sido clicada).',
     tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
@@ -22,7 +22,7 @@ const projectList = [
   },
   {
     id: 3,
-    name: 'Pixel art',
+    name: 'Pixel art 🎨',
     imagem: pixeArt,
     descricao: 'Um projeto que simula um "canva" na tela. Existem 4 cores em uma paleta geradas automaticamente na inicialização da página. É possível alterar a quantidade de "pixels", e ao clicar sobre um "pixel"  é possível pintá-lo com a cor selecionada da paleta (preta caso nenhuma tenha sido clicada).',
     tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
