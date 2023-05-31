93f259c4cf679b21950d9be2477138245a76b6d7
Arlisson N. Costa <arlisson314@gmail.com>
2023-05-31 13:53:01 -0300

    feat: Adicona prop offset para deslocamento de scroll-bar

--- src/components/NavItems/index.jsx
+++ src/components/NavItems/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 
-export default function NavItems({ Icon, children, renderSiderbar }) {
+export default function NavItems({
+  Icon, children, renderSiderbar, offset,
+}) {
   return (
     <Link
       to={children}
       spy
       smooth
-      offset={-100}
+      offset={offset ?? 0}
       duration={500}
       onClick={renderSiderbar && renderSiderbar}
       className="flex justify-center items-center space-x-1 cursor-pointer text-lg hover:text-blue-500 transition ease-in-out duration-500 text-slate-950 dark:text-gray-200 dark:hover:text-blue-500"
