5a4504c223eabc50a8b3d92e3f2abcdbdb74ee28
Arlisson N. Costa <arlisson314@gmail.com>
2023-06-12 15:49:19 -0300

    feat: move diretorio Home para raiz do page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import Home from './components/Home';
+import Home from './pages/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
--- src/test/home.test.jsx
+++ src/test/home.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import Home from '../components/Home';
+import Home from '../pages/Home';
 import cvMock from './cvMock';
 
 describe('Home component', () => {
