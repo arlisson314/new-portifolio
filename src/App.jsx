c1fb30b1710ae812d2692514478cd86a565e5194
Arlisson N. Costa <arlisson314@gmail.com>
2023-06-12 15:50:59 -0300

    feat: move diretorio About para raiz do page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Home from './pages/Home';
-import About from './components/About';
+import About from './pages/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
