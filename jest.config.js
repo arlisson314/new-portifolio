51e975b6d73f45d928a01c567a1d4ab2918815f6
Arlisson N. Costa <arlisson314@gmail.com>
2023-06-12 11:17:32 -0300

    chore: Adiciona nova config ao jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 export default {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+  transformIgnorePatterns: [
+    '/new-portifolio/src/images/curriculo\\.pdf$',
+  ],
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/mocks/fileMock.js',
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(css|less|sass|scss|pdf)$': 'identity-obj-proxy',
   },
-}
+};
